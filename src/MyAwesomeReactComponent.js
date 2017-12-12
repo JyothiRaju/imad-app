405101210421a6b0991331ef32f17dcefb6a318b
JyothiRaju <jyothirajub@gmail.com>
2017-12-12 14:28:02 +0530

    Update MyAwesomeReactComponent.js
    Made changes on Dec 12th 2017

--- src/MyAwesomeReactComponent.js
+++ src/MyAwesomeReactComponent.js
@@ -2,13 +2,28 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import "./index.css";
 alert("loaded");
+const style = {
+position:'1000000000,10,10,10',
+height:30,
+width:20,
+                                            };
+const style1 = {
+                                            position:'10000000000000,10,10,10',
+                                            height:30,
+                                            width:20,
+
+                                            //margin: '151247136450000px 60655536234500000px 10000000px 100000000000',
+                                                                                        };
+
 //const MyAwesomeReactComponent = () => ((<RaisedButton label="Sign up"/>)(<RaisedButton label="Log in"/>));
 const MyAwesomeReactComponent = () =>(<div><RaisedButton
                                             label="Sign up"
+                                            style={style}
                                             >
                                             </RaisedButton>
                                             <RaisedButton
-                                            label="Log in"></RaisedButton></div>);
+                                            label="Log in"
+                                            style={style1}></RaisedButton></div>);
 /*
 const style = {
 display: 'left',
